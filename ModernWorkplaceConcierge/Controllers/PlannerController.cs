f9b507e918a04905336dc804c7289ec22bbd81ea
Nicola Suter <32899754+nicolonsky@users.noreply.github.com>
2019-12-20 09:55:39 +0100

    parent ca365138f3e848343a8abdc92703c535bb511b38 author Nicola Suter <32899754+nicolonsky@users.noreply.github.com> 1576832139 +0100 committer Nicola Suter <nicola@nicolonsky.ch> 1577313070 +0100
    Added planner import and import UI improvements

--- ModernWorkplaceConcierge/Controllers/PlannerController.cs
+++ ModernWorkplaceConcierge/Controllers/PlannerController.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Linq;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace ModernWorkplaceConcierge.Controllers
 {
@@ -19,103 +20,172 @@ namespace ModernWorkplaceConcierge.Controllers
         // GET: Planner
         public async System.Threading.Tasks.Task<ActionResult> Index()
         {
-            // Get all plans
+            try
+            {
+                // Get all plans
+                var plans = await GraphHelper.GetplannerPlans();
 
-            var plans = await GraphHelper.GetplannerPlans();
-
-            return View(plans);
+                return View(plans);
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         [HttpPost]
         public async System.Threading.Tasks.Task<ActionResult> Import(HttpPostedFileBase file, string PlannerPlan)
         {
-            // Load JSON
+            try {
 
-            Message("Selected Planner Plan:" + PlannerPlan);
-
-            BinaryReader b = new BinaryReader(file.InputStream);
-            byte[] binData = b.ReadBytes(file.ContentLength);
-            string result = Encoding.UTF8.GetString(binData);
-
-
-            JsonReader reader = new JsonTextReader(new StringReader(result));
-            reader.DateParseHandling = DateParseHandling.None;
-            reader.DateTimeZoneHandling = DateTimeZoneHandling.Unspecified;
-            JObject trelloBoard = JObject.Load(reader);
-
-
-            foreach (JToken task in trelloBoard.SelectToken("cards"))
-            {
-                //Flash(JsonConvert.SerializeObject(task));
-
-                string dueDateTime = (string)task["due"];
-
-                string assignedUser = "";
-
-                try
-                {
-
-                    assignedUser = (string)task.SelectToken("member/name");
-                }
-                catch { }
-
-                if (!string.IsNullOrEmpty(assignedUser))
-                {
-                    Message(assignedUser);
-
-                }
-
+                // Get current planner object
+                var planner = await GraphHelper.GetplannerPlan(PlannerPlan);
                 
+                // Count imported tasks
+                int importedTasksCounter = 0;
 
-                //User user = await GraphHelper.GetUser((string)task["memberCreator/fullName"]);
+                // Get uploaded json
+                BinaryReader b = new BinaryReader(file.InputStream);
+                byte[] binData = b.ReadBytes(file.ContentLength);
+                string result = Encoding.UTF8.GetString(binData);
 
-                PlannerTask plannerTask = new PlannerTask
+                JsonReader reader = new JsonTextReader(new StringReader(result));
+                // Do not parse datetime values 
+                reader.DateParseHandling = DateParseHandling.None;
+                reader.DateTimeZoneHandling = DateTimeZoneHandling.Unspecified;
+                JObject trelloBoard = JObject.Load(reader);
+
+                // Get trello lists
+                ArrayList bucketsToCreate = new ArrayList();
+
+                foreach (JToken list in trelloBoard.SelectToken("lists"))
                 {
-                    PlanId = PlannerPlan,
-                    Title = (string)task["name"],
-                    
-                };
+                    string bucketName = (string)list["name"];
 
-                if (!string.IsNullOrEmpty(dueDateTime))
-                {
-                    plannerTask.DueDateTime = DateTimeOffset.Parse(dueDateTime);
+                    if (!bucketsToCreate.Contains(bucketName))
+                    {
+                        bucketsToCreate.Add(bucketName);
+                    }
                 }
 
-                // Add the task
-                var request = await GraphHelper.AddPlannerTask(plannerTask);
+                // Get existing planner buckets
+                IEnumerable<PlannerBucket> plannerBuckets = await GraphHelper.GetPlannerBuckets(PlannerPlan);
 
-                string attachmentName = (string)task.SelectToken("attachments[0].name");
-                string attachmentUrl = (string)task.SelectToken("attachments[0].url");
- 
-                string taskDescription = (string)task["desc"];
-
-                if (!string.IsNullOrEmpty(taskDescription) || !string.IsNullOrEmpty(attachmentUrl))
+                // Create planner bucket if not exists
+                foreach (string bucket in bucketsToCreate)
                 {
-
-                    PlannerTaskDetails plannerTaskDetails = new PlannerTaskDetails();
-
-                    plannerTaskDetails.Description = taskDescription;
-
-                    if (!string.IsNullOrEmpty(attachmentUrl) && !string.IsNullOrEmpty(attachmentName))
-                    {
-                        plannerTaskDetails.References.AddReference(attachmentUrl, attachmentName);
-                    }
-
                     try
                     {
-                        
-                        var response = await GraphHelper.AddPlannerTaskDetails(plannerTaskDetails, request.Id);
+                        if (!plannerBuckets.ToList().Where(p => p.Name == bucket).Any())
+                        {
+                            PlannerBucket plannerBucket = new PlannerBucket
+                            {
+                                Name = bucket,
+                                PlanId = PlannerPlan
+                            };
 
+                            var reponse = await GraphHelper.AddPlannerBucket(plannerBucket);
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                // Get available planner buckets
+                plannerBuckets = await GraphHelper.GetPlannerBuckets(PlannerPlan);
+
+                // create tasks
+                foreach (JToken task in trelloBoard.SelectToken("cards"))
+                {
+                    try
+                    {
+                        // Get name of the trello list which will become a planner bucket
+                        string trelloId = (string)task["idList"];
+                        string name = (string)trelloBoard.SelectToken($"$.lists[?(@.id == '{trelloId}')]")["name"];
+
+                        // Get bucketId to store tasks
+                        string bucketId = plannerBuckets.Where(p => p.Name.Equals(name)).First().Id;
+
+                        PlannerTask plannerTask = new PlannerTask
+                        {
+                            PlanId = PlannerPlan,
+                            Title = (string)task["name"],
+                            BucketId = bucketId
+                        };
+
+                        // Get completed
+                        bool isClosed = bool.Parse((string)task["closed"]);
+
+                        if (isClosed)
+                        {
+                            plannerTask.PercentComplete = 100;
+                        }
+
+                        // Get due
+                        string dueDateTime = (string)task["due"];
+
+                        if (!string.IsNullOrEmpty(dueDateTime))
+                        {
+                            plannerTask.DueDateTime = DateTimeOffset.Parse(dueDateTime);
+                        }
+
+                        // Get assigned user
+                        string assignedToId = (string)task.SelectToken("idMembers[*]");
+
+                        if (!string.IsNullOrEmpty(assignedToId))
+                        {
+                            string assignedToname = (string)trelloBoard.SelectToken($"$.members[?(@.id == '{assignedToId}')]")["fullName"];
+
+                            User user = await GraphHelper.GetUser(assignedToname);
+
+                            plannerTask.Assignments = new PlannerAssignments();
+                            plannerTask.Assignments.AddAssignee(user.Id);
+
+                        }
+
+                        // Add the task
+                        var request = await GraphHelper.AddPlannerTask(plannerTask);
+                        importedTasksCounter++;
+
+                        // Add task details like description and attachments
+
+                        string attachmentName = (string)task.SelectToken("attachments[*].name");
+                        string attachmentUrl = (string)task.SelectToken("attachments[*].url");
+                        string taskDescription = (string)task["desc"];
+
+                        if (!string.IsNullOrEmpty(taskDescription) || !string.IsNullOrEmpty(attachmentUrl))
+                        {
+                            PlannerTaskDetails plannerTaskDetails = new PlannerTaskDetails();
+
+                            if (!string.IsNullOrEmpty(taskDescription))
+                            {
+
+                                plannerTaskDetails.Description = taskDescription;
+                            }
+
+                            if (!string.IsNullOrEmpty(attachmentUrl) && !string.IsNullOrEmpty(attachmentName))
+                            {
+                                plannerTaskDetails.References = new PlannerExternalReferences();
+
+                                plannerTaskDetails.References.AddReference(attachmentUrl, attachmentName);
+                            }
+
+                            var response = await GraphHelper.AddPlannerTaskDetails(plannerTaskDetails, request.Id);
+                        }
                     }
                     catch (Exception e)
                     {
-                        Flash(e.Message);
-                    }  
+                        Flash(e.Message, e.StackTrace);
+                    }
                 }
-               // Message(JsonConvert.SerializeObject(request, Formatting.Indented));
 
+                Message("Imported: " + importedTasksCounter + " tasks to planner: " + planner.Title);
             }
-
+            catch (Exception e)
+            {
+                Flash(e.Message);
+            }
             return RedirectToAction("Index");
         }
     }
