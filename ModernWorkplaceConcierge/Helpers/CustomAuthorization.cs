a2514ffa07293e273857867a226ff398a58587aa
Nicola Suter <nicola@nicolonsky.ch>
2020-08-29 14:51:55 +0200

    Add advancedUser role

---
+++ ModernWorkplaceConcierge/Helpers/CustomAuthorization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+
+namespace ModernWorkplaceConcierge.Helpers
+{
+    public class CustomAuthorization : AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectResult("/Home/Error?message=Unauthorized&debug=You are not authorized to use this function");
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+        }
+    }
+}
